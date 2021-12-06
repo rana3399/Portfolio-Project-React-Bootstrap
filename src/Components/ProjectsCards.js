ae30407fa93467e9b282fd7e0f3688cd1e2fbe08
David Naranjo <david.naranjo@skyscanner.net>
2021-12-06 18:28:38 +0100

    fix imgages

--- src/Components/ProjectsCards.js
+++ src/Components/ProjectsCards.js
@@ -1,44 +1,53 @@
-import React from 'react';
+import React from "react";
+import coffeeImage from "../Components/project-img/coffee.jpg";
 
+const ProjectsCards = ({ projectsData }) => {
+  return (
+    <>
+      <div className="cards-main-header">
+        <h3> Some of my Projects </h3>
+      </div>
+      <div className="card-main-container row justify-content-center ">
+        {projectsData.map((project, id) => {
+          return (
+            <div
+              className="card my-card m-2 col-10 col-md-3 col-lg-3"
+              key={project.id}
+            >
+              <img
+                className="img-fluid rounded card-img-top card-img-bottom"
+                src={project.image}
+                alt="Card-cap"
+              />
 
-const ProjectsCards=({projectsData})=> {
-    return (
-        <>
-        <div className = 'cards-main-header'>
-            <h3> Some of my Projects </h3> 
-        
-        </div> 
-            <div className="card-main-container row justify-content-center ">   
-                {projectsData.map((project, id) =>{
-                    return(
-                        <div className="card my-card m-2 col-10 col-md-3 col-lg-3" key= {project.id}>
-                            <img 
-                            className="img-fluid rounded card-img-top card-img-bottom" 
-                            src={require('../Components/project-img/coffee.jpg')}
-                            alt="Card-cap"                               
-                            />
+              <div className="card-body d-flex flex-column justify-content-center ">
+                <h4 className="card-title"> {project.name} </h4>
+                <p className="card-text"> {project.description} </p>
+                <h5>React, Bootstrap, CSS </h5>
 
-                            <div className="card-body d-flex flex-column justify-content-center ">
-                                <h4 className="card-title"> {project.name} </h4>
-                                <p className="card-text"> {project.description} </p>
-                                <h5>React, Bootstrap, CSS </h5>
-                                
-                                <div className="btn-group d-flex justify-content-center m-2">
-                                    <a href={project.live} target="_blank">
-                                    <button className="btn-block my-card-btn m-2">Live website</button>
-                                    </a>
-                                    <a href={project.gitlink} target="_blank">
-                                    <button href={project.link} target="_blank" className="my-card-btn m-2">Github</button>
-                                    </a>                               
-                                </div>
-
-                            </div>
-                        </div>  
-                    )
-                })}
+                <div className="btn-group d-flex justify-content-center m-2">
+                  <a href={project.live} target="_blank" rel="noreferrer">
+                    <button className="btn-block my-card-btn m-2">
+                      Live website
+                    </button>
+                  </a>
+                  <a href={project.gitlink} target="_blank" rel="noreferrer">
+                    <button
+                      href={project.link}
+                      target="_blank"
+                      className="my-card-btn m-2"
+                    >
+                      Github
+                    </button>
+                  </a>
+                </div>
+              </div>
             </div>
-        </>
-    )
-}
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default ProjectsCards;
--- src/ProjectsMenu.js
+++ src/ProjectsMenu.js
@@ -1,55 +1,54 @@
+import touristImage from "./Components/project-img/tourism.jpg";
+import coffeeImage from "./Components/project-img/coffee.jpg";
+
 const ProjectsMenu = [
-    {
-      id: 1,
-      image: './project-img/tourism.jpg',  
-      name: " Barcelona",
-      category: "Travel & Tourism",
-      Language: "HTML, CSS, JavaScript",
-      description:
-        "I have used updated HTML and most recent version of CSS while creating this website",
-      gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
-      live: "https://rana3399.github.io/Visit-Barcelona-1/"
-    },
-  
-    {
-      id: 2,
-      image: "build/image/Rana-Ahmed-CV-01.png",
-      name: "Visit Barcelona",
-      category: "Travel & Tourism",
-      Language: "HTML, CSS, JavaScript",
-      description:
-        "I have used updated HTML and most recent version of CSS while creating this website",
-      gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
-      live: "https://rana3399.github.io/my-todo-list/"
-    
-      },
+  {
+    id: 1,
+    image: touristImage,
+    name: " Barcelona",
+    category: "Travel & Tourism",
+    Language: "HTML, CSS, JavaScript",
+    description:
+      "I have used updated HTML and most recent version of CSS while creating this website",
+    gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
+    live: "https://rana3399.github.io/Visit-Barcelona-1/",
+  },
 
-    {
-      id: 3,
-      image: "./Components/project-img/newspaper.jpg",
-      name: "Visit Barcelona",
-      category: "Travel & Tourism",
-      Language: "HTML, CSS, JavaScript",
-      description:
-        "I have used updated HTML and most recent version of CSS while creating this website",
-      gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
-      live: "https://rana3399.github.io/my-todo-list/"
-    
-      },
-      
-    {
-      id: 4,
-      image: "project-img/coffee.jpg",
-      name: "Apple Store",
-      category: "Travel & Tourism",
-      Language: "HTML, CSS, JavaScript",
-      description:
-        "I have used updated HTML and most recent version of CSS while creating this website",
-      gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
-      live: "https://rana3399.github.io/my-todo-list/"
-    
-    }
+  {
+    id: 2,
+    image: touristImage,
+    name: "Visit Barcelona",
+    category: "Travel & Tourism",
+    Language: "HTML, CSS, JavaScript",
+    description:
+      "I have used updated HTML and most recent version of CSS while creating this website",
+    gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
+    live: "https://rana3399.github.io/my-todo-list/",
+  },
 
-  ];
-  
-  export default ProjectsMenu;
+  {
+    id: 3,
+    image: touristImage,
+    name: "Visit Barcelona",
+    category: "Travel & Tourism",
+    Language: "HTML, CSS, JavaScript",
+    description:
+      "I have used updated HTML and most recent version of CSS while creating this website",
+    gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
+    live: "https://rana3399.github.io/my-todo-list/",
+  },
+
+  {
+    id: 4,
+    image: coffeeImage,
+    name: "Apple Store",
+    category: "Travel & Tourism",
+    Language: "HTML, CSS, JavaScript",
+    description:
+      "I have used updated HTML and most recent version of CSS while creating this website",
+    gitlink: "https://rana3399.github.io/Visit-Barcelona-1/",
+    live: "https://rana3399.github.io/my-todo-list/",
+  },
+];
+
+export default ProjectsMenu;
