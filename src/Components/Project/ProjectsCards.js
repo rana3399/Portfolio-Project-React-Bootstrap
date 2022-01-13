dde3ab0a81895463f152ee7b06fb7e4f2865f160
Rana_Ahmed <foster3399@gmail.com>
2022-01-13 22:56:31 +0100

    Organise Button Alingment.

--- src/Components/Project/ProjectsCards.js
+++ src/Components/Project/ProjectsCards.js
@@ -6,15 +6,18 @@ const ProjectsCards=({project})=> {
             <div className="card m-2 col-lg-3 col-md-4 col-sm-10 my-class " key= {project.id}>
             
                 <img 
-                className="img-fluid mt-2 rounded card-img-top card-img-bottom" 
+                className="img-fluid mt-2 rounded" 
+                style={{maxHeight: "200px"}}
                 src= {project.image}
                 alt="Card-cap"                               
                 />
-
                 <div className="card-body d-flex flex-column justify-content-center ">
-                    <h4 className="card-title"> {project.name} </h4>
-                    <p className="card-text"> {project.description} </p>
-                    <h5>{project.language} </h5>
+                    <div className='my-card-body' style={{minHeight: "250PX"}}  >
+
+                        <h4 className="card-title"> {project.name} </h4>
+                        <p className="card-text"> {project.description} </p>
+                        <h5>{project.language} </h5>
+                    </div>                
                     
                     <div className="btn-group d-flex justify-content-center m-2">
                         <a href={project.live} target="_blank">
@@ -24,7 +27,6 @@ const ProjectsCards=({project})=> {
                         <button href={project.link} target="_blank" className="my-card-btn m-2">Github</button>
                         </a>                               
                     </div>
-
                 </div>
             </div>       
         </>
